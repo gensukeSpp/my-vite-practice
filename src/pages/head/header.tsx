1132175ab3456a06632b2fb1a43b42eb5cce93e6
unknown <gensukeSpp>
2022-11-11 12:17:34 +0900

    Plus header page.

---
+++ src/pages/head/header.tsx
@@ -0,0 +1,11 @@
+export const Header: React.FC<{}> = () => {
+    <header>
+        <div id="head-item">
+            <div className="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </header>
+}
