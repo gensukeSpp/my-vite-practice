59ea53191eb2dbe161efc00bd3d5ab6da5af5733
unknown <gensukeSpp>
2022-11-21 13:12:43 +0900

    Map render menu in Header.

--- src/pages/head/header.tsx
+++ src/pages/head/header.tsx
@@ -1,12 +1,23 @@
 
-type MenuPropsgit = {
-    children: React.ReactNode,
+type MenuProps = {
+    toLink: ["", "about", "menu", "ranking", "contact"],
+    children: React.ReactNode
 }
 
+const LinkList = (props: MenuProps) => {
+    const linkLength = props.toLink.length;
+    for(let i = 1; i <= linkLength; i++) {
+        {props.toLink.map((link) => {
+            return `<li><a href="./${link}"><img src="./img/navi_0${link[linkLength]}.jpg" alt="トップページ" /></a></li>`
+        })}
+    }
+}
 export const MenuInHeader = () => {
-    <div className="hamburger">
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
+    <header>
+        <div id="head-contents">
+        <div><img src="./img/logo.png" alt="スウィーツオンラインストア" /></div>
+            <ul>
+            </ul>
+        </div>
+    </header>
 }
