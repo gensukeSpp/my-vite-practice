a7663f5f80cc5dd6808545698d6ac91a52149fb7
unknown <gensukeSpp>
2022-11-28 10:23:52 +0900

    There is still Props.

--- src/pages/head/header.tsx
+++ src/pages/head/header.tsx
@@ -1,22 +1,27 @@
+import { JSXElement } from "@babel/types";
 
 type MenuProps = {
     toLink: ["", "about", "menu", "ranking", "contact"],
     children: React.ReactNode
 }
 
-const LinkList = (props: MenuProps) => {
+const HeaderLinkList = (props: MenuProps):JSX.Element => {
     const linkLength = props.toLink.length;
+    let listTag;
     for(let i = 1; i <= linkLength; i++) {
         {props.toLink.map((link) => {
-            return `<li><a href="./${link}"><img src="./img/navi_0${link[linkLength]}.jpg" alt="トップページ" /></a></li>`
+            listTag.push(`<li><a href="./${link}"><img src="./img/navi_0${link[linkLength]}.jpg" /></a></li>`)
         })}
     }
+    return listTag;
 }
+
 export const MenuInHeader = () => {
     <header>
         <div id="head-contents">
         <div><img src="./img/logo.png" alt="スウィーツオンラインストア" /></div>
             <ul>
+                <HeaderLinkList 
             </ul>
         </div>
     </header>
