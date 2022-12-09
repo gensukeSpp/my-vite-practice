94dd9ad78fcbf4913d11168f8df23a8db30839b2
unknown <gensukeSpp>
2022-12-09 10:48:51 +0900

    Body.json(): Promise<any> is successkeyword.

--- src/pages/topics/main.tsx
+++ src/pages/topics/main.tsx
@@ -1,41 +1,43 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 type TopicProp = {
     content: string,
     link: string
 }
-type Props = {
-    Topics: Dispatch<SetStateAction<TopicProp>>
-}
+// type Props = {
+//     Topics: Dispatch<SetStateAction<TopicProp>>
+// }
 
 const url = "https://my-json-server.typicode.com/gensukeSpp/sweets_fake/topics";
-const fetchData = async (): Promise<Props> => {
-    const data = await fetch(url, {method: 'GET'});
-    const json = data.json();
-    return json;
-}
-// const fetchData/*: Promise<Props>*/ = fetch(url, {
-//         method: 'GET'})
-//     .then(res => res.json());
+// const fetchData = async () => {
+//     const data: Response = await fetch(url, {
+//         method: 'GET'
+//     })
+//     const json = data.json();
+//     return json;
+// }
+const fetchData/*: Promise<Props>*/ = fetch(url, {
+        method: 'GET'})
+    .then(res => res.json());
 
 const TopicsFetch = () => {
 
     const [content, setContent] = useState("");
     const [link, setLink] = useState("");
     const [isLosding, setIsLoading] = useState(false);
-    const [topics, setTopics] = useState([]);
+    const [topics, setTopics] = useState<TopicProp[]>([]);
 
     // let json;
     useEffect(() => {
-    //     const f = async () => {
-    //         setIsLoading(true);
-    //         const json = await fetchData;
-    //         setTopics(json);
-    //         setIsLoading(false);
-    //     }
-    //    f();
-        fetchData()
-            .then((json) => setTopics(json))
+        const f = async () => {
+            setIsLoading(true);
+            const json = await fetchData;
+            setTopics(json);
+            setIsLoading(false);
+        }
+       f();
+        // fetchData()
+        //     .then((json) => setTopics(json))
     }, []);
 
     // const TypicodeTodo: Promise<TopicProp[]> = fetch("https://my-json-server.typicode.com/gensukeSpp/sweets_fake/topics", {
@@ -79,3 +81,9 @@ export const TopicsArea = () => {
         </main>
     )
 }
+// {　 // 送信先URL
+//         method: 'post', // 通信メソッド
+//         headers: {
+//             'Content-Type': 'application/json' // JSON形式のデータのヘッダー
+//         },
+//         body: JSON.stringify(data) // JSON形式のデータ
